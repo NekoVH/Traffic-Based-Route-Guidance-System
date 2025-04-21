76e22e33f01e83875ed209a14a69a05adf7bbcb2
Cao Minh Vu <vucaominh1408@gmail.com>
2025-04-21 14:38:54 +1000

    There should be activation types for each layers

--- main.py
+++ main.py
@@ -1,5 +1,12 @@
 from neural_network import *
 
-network = NeuralNetwork([2,3,2])
+network = NeuralNetwork([2,3,2], ["relu", "sigmoid"])
 
-print(f"Output of the network is: {network.calculate_outputs([0.8, 0.9])}")
+inputs = [0.5, 0.6]
+
+print(f"Inputs: {inputs}\n")
+for layer in network.layers:
+	print(f"Weights: {layer.weights}")
+	print(f"Biases: {layer.biases}")
+	print(f"Activation type: {layer.activation_type}\n")
+print(f"Output of the network is: {network.feed_forward(inputs)}")
--- neural_network.py
+++ neural_network.py
@@ -1,40 +1,52 @@
 import numpy as np
+from dataclasses import dataclass
 
-def activation_function(activation_type, value):
+def activation_function(activation_type, values):
 	match activation_type:
 		case "sigmoid":
-			return 1 / (1 + np.exp(-value))
+			return 1 / (1 + np.exp(-values))
 		case "relu":
-			return max(0, value)
+			return np.maximum(0, values)
+
+@dataclass
+class DataPoint:
+	inputs: np.ndarray
+	expected_outputs: np.ndarray
+	label: int
+
+	def one_hot_encoding(self):
+		one_hot = np.zeros(len(inputs))
+		one_hot[label] = 1
+		return one_hot
 
 class Layer():
-	def __init__(self, num_nodes_in, num_nodes_out, activation_type):
+	def __init__(self, num_nodes_in: int, num_nodes_out: int, activation_type: str):
 		self.num_nodes_in = num_nodes_in
 		self.num_nodes_out = num_nodes_out
 		self.activation_type = activation_type
 
-		self.weights = np.ones((num_nodes_in, num_nodes_out))
-		self.biases = np.ones((num_nodes_out,))
+		self.weights = np.random.rand(num_nodes_in, num_nodes_out)
+		self.biases = np.random.rand(num_nodes_out)
 
-	def calculate_outputs(self, inputs):
+	def feed_forward(self, inputs: list) -> np.ndarray:
 		activations = np.zeros((self.num_nodes_out,))
 
 		for node_out in range(0, self.num_nodes_out):
 			weighted_input = self.biases[node_out]
 			for node_in in range(0, self.num_nodes_in):
 				weighted_input += inputs[node_in] * self.weights[node_in, node_out]
-			activations[node_out] = activation_function(self.activation_type,weighted_input)
+			activations[node_out] = weighted_input
 
+		activations = activation_function(self.activation_type, activations)
 		return activations
 
-
 class NeuralNetwork():
-	def __init__(self, layer_sizes, activation_type="sigmoid"):
+	def __init__(self, layer_sizes: list, activation_types: str):
 		self.layers = []
 		for i in range(0, len(layer_sizes) - 1):
-			self.layers.append(Layer(layer_sizes[i], layer_sizes[i+1], activation_type))
+			self.layers.append(Layer(layer_sizes[i], layer_sizes[i+1], activation_types[i]))
 
-	def calculate_outputs(self, inputs):
+	def feed_forward(self, inputs: list):
 		for layer in self.layers:
-			inputs = layer.calculate_outputs(inputs)
+			inputs = layer.feed_forward(inputs)
 		return inputs

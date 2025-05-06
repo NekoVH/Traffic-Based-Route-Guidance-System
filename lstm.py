6a1852b93f8c702295d7b469ca77bac913b641fc
Cao Minh Vu <vucaominh1408@gmail.com>
2025-05-06 20:21:38 +1000

    Done LSTM and training, need to rescale loss

--- lstm.py
+++ lstm.py
@@ -2,21 +2,19 @@ import torch
 import torch.nn as nn
 
 class LSTM(nn.Module):
-    def __init__(self, input_size=1, hidden_size=64, num_layers=2, device="cpu"):
+    def __init__(self, input_size=7, hidden_size=64, num_layers=1, output_dim=1, dropout_prob=0.2):
         super(LSTM, self).__init__()
 
         self.hidden_size = hidden_size
         self.num_layers = num_layers
 
-        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True)
-        self.fc1 = nn.Linear(hidden_size, 1)
-        self.device = device
+        self.lstm = nn.LSTM(input_size, hidden_size, num_layers, batch_first=True, dropout=dropout_prob)
+        self.fc = nn.Linear(hidden_size, output_dim)
 
-    def forward(self, x):
-        h0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
-        c0 = torch.zeros(self.num_layers, x.size(0), self.hidden_size).to(self.device)
-
-        out, _ = self.lstm(x, (h0, c0))
-        out = out.reshape(out.size(0), -1)
-        out = self.fc1(out)
+    def forward(self, X):
+        hidden_states = torch.zeros(self.num_layers, X.size(0), self.hidden_size).requires_grad_()
+        cell_states = torch.zeros(self.num_layers, X.size(0), self.hidden_size).requires_grad_()
+        out, (hn, cn) = self.lstm(X, (hidden_states.detach(), cell_states.detach()))
+        out = out[:, -1, :]
+        out = self.fc(out)
         return out

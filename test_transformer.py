6ac41119f77add511d5f36dd1d48cb18cf1b24df
Viet Hoang Pham <104506968@student.swin.edu.au>
2025-05-11 18:04:29 +1000

    Integrate the model with the training class

---
+++ test_transformer.py
@@ -0,0 +1,44 @@
+﻿import torch
+from torch import optim, nn
+from torch.utils.data import TensorDataset, DataLoader
+
+from training import Optimization
+from transformer import Transformer
+from utils.data_processing import process_data, read_excel
+
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+flow_dataset, flow_rescaler = process_data(read_excel("datasets/Scats Data October 2006.xls", sheet_name="Data", header=1))
+
+X_train = torch.tensor(flow_dataset.X_train, dtype=torch.float32)
+X_test = torch.tensor(flow_dataset.X_test, dtype=torch.float32)
+y_train = torch.tensor(flow_dataset.y_train, dtype=torch.float32)
+y_test = torch.tensor(flow_dataset.y_test, dtype=torch.float32)
+
+training_data = TensorDataset(X_train, y_train)
+test_data = TensorDataset(X_test, y_test)
+
+# Hyperparameters
+input_dim = 7
+output_dim = 1
+hidden_dim = 64
+layer_dim = 1
+batch_size = 64
+dropout = 0.2
+n_epochs = 1000
+learning_rate = 1e-3
+weight_decay = 1e-6
+
+# Load Dataset to DataLoader
+# Transformer expects (batch, seq_len, input_dim)
+train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=False, drop_last=True)
+test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False, drop_last=True)
+
+model = Transformer().to(device)
+
+loss_fn = nn.MSELoss(reduction="mean")
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+opt = Optimization(model=model, loss_fn=loss_fn, optimizer=optimizer, rescaler=flow_rescaler)
+opt.train(train_dataloader, n_epochs=n_epochs, n_features=input_dim)
+opt.plot_losses()
+

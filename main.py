617e6fdb817f2bb844ebb2add60786e75e0f95b8
Cao Minh Vu <vucaominh1408@gmail.com>
2025-05-13 17:42:21 +1000

    Merge 'refactor' into 'model-plot': Added command line arguments for epochs and model selection

--- main.py
+++ main.py
@@ -1,24 +1,74 @@
-from torch.utils.data import TensorDataset, DataLoader
-import torch.nn as nn
 import torch
-from models import LSTM
+from torch import optim, nn
+from torch.utils.data import TensorDataset, DataLoader
 
-from utils.data_processing import read_excel, process_data
+from training import Optimization
+from transformer import Transformer
+from gru import GRU
+from lstm import LSTM
+from utils.data_processing import process_data, read_excel
 
-lstm = LSTM()
+from hyperparams import *
 
-df = read_excel(
-    filename="datasets/Scats Data October 2006.xls",
-    sheet_name="Data",
-    header=1
+import argparse
+
+parser = argparse.ArgumentParser(
+    prog="tbrgs",
+    description="Traffic-based Route Guidance System",
+    usage="%(prog)s [options]"
 )
 
-flow_dataset = process_data(df)
+parser.add_argument('-e', '--epochs', help="Number of training epochs. Default = 1000", default=1000)
+parser.add_argument('-m', '--model', help="Select neural network model. Either 'lstm', 'gru', or 'transformer'")
 
-X_train = torch.FloatTensor(flow_dataset.X_train)
-X_test = torch.FloatTensor(flow_dataset.X_test)
-y_train = torch.FloatTensor(flow_dataset.y_train)
-y_test = torch.FloatTensor(flow_dataset.y_test)
+args = parser.parse_args()
+if args.model is None:
+    parser.print_help()
+    exit()
 
-print(f"{X_train} {X_train.shape}\n")
-print(f"{y_train} {y_train.shape}")
+device = torch.device("cuda" if torch.cuda.is_available() else "cpu")
+flow_dataset, flow_rescaler = process_data(read_excel("datasets/Scats Data October 2006.xls", sheet_name="Data", header=1))
+
+X_train = torch.tensor(flow_dataset.X_train, dtype=torch.float32)
+X_val = torch.tensor(flow_dataset.X_val, dtype=torch.float32)
+X_test = torch.tensor(flow_dataset.X_test, dtype=torch.float32)
+
+y_train = torch.tensor(flow_dataset.y_train, dtype=torch.float32)
+y_val = torch.tensor(flow_dataset.y_val, dtype=torch.float32)
+y_test = torch.tensor(flow_dataset.y_test, dtype=torch.float32)
+
+training_data = TensorDataset(X_train, y_train)
+validation_data = TensorDataset(X_val, y_val)
+test_data = TensorDataset(X_test, y_test)
+
+# Load Dataset to DataLoader
+train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=False, drop_last=True)
+validation_dataloader = DataLoader(validation_data, batch_size=batch_size, shuffle=False, drop_last=True)
+test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False, drop_last=True)
+
+match args.model:
+    case 'lstm':
+        model = LSTM()
+    case 'gru':
+        model = GRU()
+    case 'transformer':
+        model = Transformer()
+    case _:
+        exit("No such model")
+
+loss_fn = torch.nn.MSELoss(reduction='mean')
+optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
+
+opt = Optimization(
+    model=model,
+    optimizer=optimizer,
+    loss_fn=loss_fn,
+    epochs=int(args.epochs),
+    rescaler=flow_rescaler,
+    device=device,
+)
+
+opt.train(train_dataloader, validation_dataloader, n_features=input_dim)
+opt.plot_losses()
+
+opt.evaluate(test_dataloader, n_features=input_dim)

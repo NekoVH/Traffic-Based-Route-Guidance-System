233c844332458b40d0ded4a608b383cc680b7f7b
Cao Minh Vu <vucaominh1408@gmail.com>
2025-05-13 15:05:38 +1000

    Included validation dataset to test_lstm.py

--- test_lstm.py
+++ test_lstm.py
@@ -13,9 +13,12 @@ X_train = torch.tensor(flow_dataset.X_train, dtype=torch.float32)
 X_test = torch.tensor(flow_dataset.X_test, dtype=torch.float32)
 y_train = torch.tensor(flow_dataset.y_train, dtype=torch.float32)
 y_test = torch.tensor(flow_dataset.y_test, dtype=torch.float32)
+X_val = torch.tensor(flow_dataset.X_val, dtype=torch.float32)
+y_val = torch.tensor(flow_dataset.y_val, dtype=torch.float32)
 
 training_data = TensorDataset(X_train, y_train)
 test_data = TensorDataset(X_test, y_test)
+validation_data = TensorDataset(X_val, y_val)
 
 # Hyperparameters
 input_dim = 7
@@ -31,7 +34,7 @@ weight_decay =1e-6
 # Load Dataset to DataLoader
 train_dataloader = DataLoader(training_data, batch_size=batch_size, shuffle=False, drop_last=True)
 test_dataloader = DataLoader(test_data, batch_size=batch_size, shuffle=False, drop_last=True)
-
+validation_dataloader = DataLoader(validation_data, batch_size=batch_size, shuffle=False, drop_last=True)
 
 
 model = LSTM()
@@ -40,5 +43,5 @@ loss_fn = nn.MSELoss(reduction="mean")
 optimizer = optim.Adam(model.parameters(), lr=learning_rate, weight_decay=weight_decay)
 
 opt = Optimization(model=model, loss_fn=loss_fn, optimizer=optimizer, rescaler=flow_rescaler)
-opt.train(train_dataloader, n_epochs=n_epochs, n_features=input_dim)
+opt.train(train_dataloader, validation_dataloader, n_epochs=n_epochs, n_features=input_dim)
 opt.plot_losses()
